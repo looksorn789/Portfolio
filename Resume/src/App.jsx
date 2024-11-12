08c7cb76b09bb08b511e55c8c156601d75e816aa
looksorn789 <patandjisoo@gmail.com>
2024-11-11 23:52:38 -0800

    testing routing

--- Resume/src/App.jsx
+++ Resume/src/App.jsx
@@ -8,11 +8,11 @@ import Socials from "./pages/Socials";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/Resume">
       <Header />
       <main style={{ marginTop: "70px" }}>
         <Routes>
-          <Route path="/Resume/" element={<Home />} /> {/* Default Home Page */}
+          <Route path="/" element={<Home />} /> {/* Default Home Page */}
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/socials" element={<Socials />} />
